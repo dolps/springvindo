f8258776a26a5892a5439ad4db09f982b5910af0
dolplads@gmail.com <broCode123>
2018-10-03 21:36:24 +0200

    added lombok for simplicity

--- src/main/java/com/dolp/vindo/domain/Measurement.java
+++ src/main/java/com/dolp/vindo/domain/Measurement.java
@@ -1,6 +1,9 @@
 package com.dolp.vindo.domain;
 
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
@@ -8,6 +11,8 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import java.util.Date;
 
+@Data
+@NoArgsConstructor
 @XmlRootElement(name = "Measurement")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Measurement {
@@ -33,138 +38,4 @@ public class Measurement {
     private double directionStDev;
     @XmlElement(name = "Temperature1")
     private double temperature;
-
-
-    /**
-     * <Measurement>
-     * <DataID>3971879</DataID>
-     * <StationID>3</StationID>
-     * <Time>2018-10-03T18:12:38+00:00</Time>
-     * <WindAvg>3.1</WindAvg>
-     * <WindVectorAvg>-999</WindVectorAvg>
-     * <WindStDev>1.1</WindStDev>
-     * <WindMax>6.1</WindMax>
-     * <WindMin>1.5</WindMin>
-     * <DirectionAvg>7</DirectionAvg>
-     * <DirectionVectorAvg>7</DirectionVectorAvg>
-     * <DirectionStDev>16.1</DirectionStDev>
-     * <Temperature1>7.4</Temperature1>
-     * <Temperature2>-999</Temperature2>
-     * <Light>-999</Light>
-     * <Battery>4.033</Battery>
-     * <Pressure/>
-     * <Humidity/>
-     * </Measurement>
-     */
-
-    public Measurement() {
-    }
-
-    public int getDataId() {
-        return dataId;
-    }
-
-    public void setDataId(int dataId) {
-        this.dataId = dataId;
-    }
-
-    public int getStationId() {
-        return stationId;
-    }
-
-    public Date getTime() {
-        return time;
-    }
-
-    public void setTime(Date time) {
-        this.time = time;
-    }
-
-    public double getWindAvg() {
-        return windAvg;
-    }
-
-    public void setWindAvg(double windAvg) {
-        this.windAvg = windAvg;
-    }
-
-    public double getWindStDev() {
-        return windStDev;
-    }
-
-    public void setWindStDev(double windStDev) {
-        this.windStDev = windStDev;
-    }
-
-    public double getWindMax() {
-        return windMax;
-    }
-
-    public void setWindMax(double windMax) {
-        this.windMax = windMax;
-    }
-
-    public double getWindMin() {
-        return windMin;
-    }
-
-    public void setWindMin(double windMin) {
-        this.windMin = windMin;
-    }
-
-    public double getDirectionAvg() {
-        return directionAvg;
-    }
-
-    public void setDirectionAvg(double directionAvg) {
-        this.directionAvg = directionAvg;
-    }
-
-    public double getDirectionVectorAvg() {
-        return directionVectorAvg;
-    }
-
-    public void setDirectionVectorAvg(double directionVectorAvg) {
-        this.directionVectorAvg = directionVectorAvg;
-    }
-
-    public double getDirectionStDev() {
-        return directionStDev;
-    }
-
-    public void setDirectionStDev(double directionStDev) {
-        this.directionStDev = directionStDev;
-    }
-
-    public double getTemperature() {
-        return temperature;
-    }
-
-    public void setTemperature(double temperature) {
-        this.temperature = temperature;
-    }
-
-    public void setStationId(int stationId) {
-
-        this.stationId = stationId;
-    }
-
-    @Override
-    public String toString() {
-        return "Measurement{" +
-            "dataId=" + dataId +
-            ", stationId=" + stationId +
-            ", time=" + time +
-            ", windAvg=" + windAvg +
-            ", windStDev=" + windStDev +
-            ", windMax=" + windMax +
-            ", windMin=" + windMin +
-            ", directionAvg=" + directionAvg +
-            ", directionVectorAvg=" + directionVectorAvg +
-            ", directionStDev=" + directionStDev +
-            ", temperature=" + temperature +
-            '}';
-    }
-
-
 }
